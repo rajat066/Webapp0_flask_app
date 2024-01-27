4022fd988e01bb72876011af84c7ee98c2fd5bcc
RAJAT KUMAR UPADHYAYA <rajatupadhyaya066@gmail.com>
2024-01-27 23:22:40 +0530

    Initial - Empty
    Deleting existing unused/unnecessary files

--- src/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-proj = Flask(__name__)
-
-@proj.route('/')
-def index():
-    return 'Welcome to flask login application'
-
-if __name__ == "__main__":
-    proj.run(debug = True, port=701)
