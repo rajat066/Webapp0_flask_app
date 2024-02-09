84c88b71b9f123b4832881f044feced43e2284a3
RAJAT KUMAR UPADHYAYA <rajatupadhyaya066@gmail.com>
2024-02-09 13:56:43 +0530

    End of Initial layout
    Completed initial design

--- Websites/__init__.py
+++ Websites/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pyodbc
+
+# # db = SQLAlchemy()
+# #DB_NAME = "database.db"
+# conn = pyodbc.connect('Driver={SQL Server};'
+#                       'Server=DESKTOP-7OPOU5G;'
+#                       'Database=QuickkartDB;'
+#                       'Trusted_Connection=yes;')
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'jfsljfajfal alsfjaskljfa'
+    # app.config['SQLALCHEMY_DATAASE_URI'] = 'sql:///{conn}'
+    # db.init_app(app)
     
     from .views import views
     from .auth import auth
--- Websites/auth.py
+++ Websites/auth.py
@@ -1,16 +1,35 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    data = request.form
+    print(data)
+    return render_template("login.html", text="testing", bollean = False)
 
 @auth.route('/logout')
 def logout():
     return render_template("logout.html")
 
-@auth.route('sign_up')
+@auth.route('sign_up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            flash('Email must be greater than 3 charcter.', category='error')
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 1 charcter.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password is too short.', category='error')
+        else:
+            flash('Account created successfully.', category='success')
+
+
     return render_template("sign_up.html")
 
